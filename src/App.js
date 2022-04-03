d0d523ebcd1e3879c1af9e10c6bde4f91be62633
christianvaldovinos <christianvaldovinos01@gmail.com>
2022-04-02 22:07:40 -0700

    Editing App to verify changes work

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
                 <div id={"navButtons"}>
                     <div id={"button1"} className={"buttons"}>Home</div>
                     <div id={"button2"} className={"buttons"}>About</div>
-                    <div id={"button3"} className={"buttons"}>Pricing</div>
+                    <div id={"button3"} className={"buttons"}>Prices</div>
                     <div id={"button4"} className={"buttons"}>Blog</div>
                     <div id={"button5"} className={"buttons"}>Contact</div>
                 </div>
