d5956bcb2770fcd82910b4083a95c9869482d649
christianvaldovinos <cvaldovinos123@gmail.com>
2022-12-18 20:27:13 -0800

    Changing punctuation

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,7 @@ function Home() {
     return (
         <div id="homeDiv" className="box">
             <div id="homeText">
-                <h1>Hello, my name is <span id="name">Christian</span>.</h1>
+                <h1>Hello, my name is <span id="name">Christian</span>!</h1>
                 <p id="subHeadingHome">Welcome to my page!</p>
                 <a id="resumeButton" href="./Resume.pdf" download>
                     Download Resume
